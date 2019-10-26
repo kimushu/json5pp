6c12d9be582cc9ef57ee561dbeba8304a11ba9f8
kimu_shu <alfvegardrisc@gmail.com>
2019-10-26 20:19:59 +0900

    Fix test output

--- test/pass.cpp
+++ test/pass.cpp
@@ -24,22 +24,30 @@ int main(int argc, char *argv[])
     std::ifstream in(argv[1], std::ios::binary);
     auto value = json5pp::PARSE_METHOD(in);
     try {
-      if (!verify(value)) { throw -1; }
-      if (argc == 3) {
-        std::ofstream out(argv[2], std::ios::binary);
-        out << json5pp::rule::space_indent<2>() << value;
+      const auto result = verify(value);
+      if (result) {
+        if (argc == 3) {
+          std::ofstream out(argv[2], std::ios::binary);
+          out << json5pp::rule::space_indent<2>() << value;
+        }
+        std::cout << "\"[PASS] parse and match succeeded.\"," << std::endl;
+        std::exit(0);
+      } else {
+        std::cout << "\"[FAIL] parse succeeded but match failed.\"," << std::endl;
+        std::exit(1);
       }
-      std::cout << "\"[PASS] parse and match succeeded.\"," << std::endl;
-      std::exit(0);
+    } catch (const std::exception& e) {
+      std::cout << "\"[FAIL] verify failed with unexpected error: " << e.what() << "\"," << std::endl;
+      std::exit(2);
     } catch (...) {
-      std::cout << "\"[FAIL] parse succeeded but match failed.\"," << std::endl;
-      std::exit(1);
+      std::cout << "\"[FAIL] verify failed with unexpected error.\"," << std::endl;
+      std::exit(2);
     }
   } catch (const std::exception& e) {
-    std::cout << "\"[FAIL] unexpected error: " << e.what() << "\"," << std::endl;
+    std::cout << "\"[FAIL] parse failed with unexpected error: " << e.what() << "\"," << std::endl;
     std::exit(2);
   } catch (...) {
-    std::cout << "\"[FAIL] unexpected error.\"," << std::endl;
+    std::cout << "\"[FAIL] parse failed with unexpected error.\"," << std::endl;
     std::exit(2);
   }
 }
